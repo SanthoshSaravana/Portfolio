ffb377084caf93918932ad2e2321ccc812f2601c
Santhosh Saravana <109412677+SanthoshSaravana@users.noreply.github.com>
2024-12-28 20:22:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed= new Typed('#autotype', {
+    strings:["Front-End Developer" , "Mobile App Developer" , "Programmer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
